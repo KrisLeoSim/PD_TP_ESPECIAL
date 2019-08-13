290456ff1c48d3b72ecaf553e76ee41958bf1f7e
SCrolem <darkxield@gmail.com>
2019-08-13 01:05:26 +0100

    commit das mudanças do cliente rca

--- viagens_cliente-rca/src/viagens_cliente/menu/Operador.java
+++ viagens_cliente-rca/src/viagens_cliente/menu/Operador.java
@@ -5,7 +5,9 @@
  */
 package viagens_cliente.menu;
 
+import java.util.ArrayList;
 import logica.ControladorRemote;
+import regras.negocio.entidade.UtilizadorPojo;
 
 /**
  *
@@ -30,6 +32,49 @@ public class Operador extends Menu {
         System.out.println("  0  - logout");
         System.out.println("--------------------------");
     }
+    
+     protected void imprimeMenuGestaoUtilizadores() {
+        System.out.println();
+        System.out.println("--------------------------");
+        System.out.println("Gestão de utilizadores");
+        System.out.println("  1  - Adicionar Utilizador");
+        System.out.println("  2  - Remover Utilizador");
+        System.out.println("  3  - Pedidos de registos pendentes");
+        System.out.println("--------------------------");
+        System.out.println("  0  - Sair");
+        System.out.println("--------------------------");
+    }
+    
+    public void cicloDeVidaGestaoUtilizadores() {
+      
+        try {         
+            while(naoSair) {
+                imprimeMenuGestaoUtilizadores();
+                opcao = obtemOpcaoMenu(4);
+
+                switch(opcao) {
+                    case 1:
+                        ListaDeUtilizadoresComRegistoPendente();
+                        break;
+                    case 2:
+                       //obtemVoos();
+                        break;      
+                    case 3:
+                         ListaDeUtilizadoresComRegistoPendente();
+                        break;
+                    case 0:
+                       //logout();
+                    naoSair = false;
+                    break;                        
+                    default:                      
+                        break;
+                }
+            }
+        } catch(Exception e) {
+            e.printStackTrace();        
+        } 
+    }
+    
 
     @Override
     public int cicloDeVida() {
@@ -41,7 +86,7 @@ public class Operador extends Menu {
 
                 switch(opcao) {
                     case 1:
-                     //ListaDeUtilizadoresComRegistoPendente();
+                        cicloDeVidaGestaoUtilizadores();
                         break;
                     case 2:
                        //obtemVoos();
@@ -81,5 +126,36 @@ public class Operador extends Menu {
     //poder selecionar e ver perfil? e editar os dados? (da muito trablaho ... com interface web é bem mais simples ver se é
     //necesario)
     //}
+    
+    public void ListaDeUtilizadoresComRegistoPendente(){
+        
+        try {
+            System.out.println();
+            System.out.println("--------------------------");
+            System.out.println("  UTILIZADORES QUE AGUARDAM APROVAÇÂO ");
+            System.out.println("--------------------------");
+            
+            ArrayList<UtilizadorPojo> lista_utilizadores= controladorEJB.listaContasQueAguardamAprovação();
+            int i = 1;
+            for(UtilizadorPojo utilizadorPojo : lista_utilizadores){
+                
+            System.out.println(" " + i + "-" + utilizadorPojo.getUsername() + " tipo:" + utilizadorPojo.getTipoUser());
+            i++;
+            
+            }
+            System.out.println("0 - sair");
+            System.out.println("--------------------------");
+            
+           // System.out.print("inserir o numero do utilizador: ");
+            //String username = sc.nextLine();
+            //System.out.println();
+     
+            
+        } catch(Exception e) {
+            System.out.println("ERRO: "+e.getMessage());
+            e.printStackTrace(System.out);
+        }
+  
+    }
     
 }
