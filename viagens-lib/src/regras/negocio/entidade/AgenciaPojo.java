7d1c0b660f4194cc1b43f53669915d9d73fa4535
SCrolem <darkxield@gmail.com>
2019-08-27 00:37:19 +0100

    commit das mudanças do viagens lib v0.4

--- viagens-lib/src/regras/negocio/entidade/AgenciaPojo.java
+++ viagens-lib/src/regras/negocio/entidade/AgenciaPojo.java
@@ -31,7 +31,7 @@ public class AgenciaPojo implements Serializable {
     }
 
     public AgenciaPojo() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       
     }
 
     public int getIdAgencia() {
